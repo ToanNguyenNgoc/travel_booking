83b91f70dfa9bc7ac75481b927008779b8672abf
nguyentoan06 <toan@myspa.vn>
2023-02-23 15:47:33 +0700

    [UPDATE] config cors all origin 2

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,9 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: ['*'],
+    origin: [
+      'http://localhost:3000'
+    ],
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATH'],
     credentials: true
   })
