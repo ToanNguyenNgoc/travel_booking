6136f87d1ef2dfa006195da277280478eee15bc2
nguyentoan06 <toan@myspa.vn>
2023-02-23 14:46:13 +0700

    [UPDATE] database config 3

--- db/data-source.ts
+++ db/data-source.ts
@@ -4,13 +4,22 @@ import { DataSourceOptions, DataSource } from 'typeorm'
 // const dotConfig = new ConfigService()
 export const databaseConfig = (configService) => {
     return {
-        type: configService.get('TYPEORM_TYPE') as any,
-        host: configService.get('TYPEORM_HOST') as any,
-        port: configService.get('TYPEORM_PORT') as any,
-        username: configService.get('TYPEORM_USERNAME'),
-        password: configService.get('TYPEORM_PASSWORD') as any,
-        database: configService.get('TYPEORM_DATABASE') as any,
-        ssl: configService.get('TYPEORM_SSL'),
+        // type: configService.get('TYPEORM_TYPE') as any,
+        // host: configService.get('TYPEORM_HOST') as any,
+        // port: configService.get('TYPEORM_PORT') as any,
+        // username: configService.get('TYPEORM_USERNAME'),
+        // password: configService.get('TYPEORM_PASSWORD') as any,
+        // database: configService.get('TYPEORM_DATABASE') as any,
+        // ssl: configService.get('TYPEORM_SSL'),
+
+        type: 'postgres',
+        host: 'ep-fancy-cloud-261497.ap-southeast-1.aws.neon.tech',
+        port: 5432,
+        username: '5751071044',
+        password: '1qjDGJ6EtFCS',
+        database: 'neondb',
+        ssl: true,
+
         synchronize: true,
         autoLoadEntities: true,
         entities: ['dist/**/*.entity.js'],
