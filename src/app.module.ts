da08d290271efdd608c0b1aa61da4eac935fc5cc
nguyentoan06 <toan@myspa.vn>
2023-02-24 08:38:22 +0700

    [FIX] config db

--- src/app.module.ts
+++ src/app.module.ts
@@ -19,7 +19,19 @@ import { AbilitiesGuard } from './ability/abilities.guard';
       imports: [ConfigModule],
       inject: [ConfigService],
       useFactory: async (configService: ConfigService) => {
-        return databaseConfig(configService)
+        return {
+          type: configService.get('TYPEORM_TYPE') as any,
+          host: configService.get('TYPEORM_HOST') as any,
+          port: configService.get('TYPEORM_PORT') as any,
+          username: configService.get('TYPEORM_USERNAME'),
+          password: configService.get('TYPEORM_PASSWORD') as any,
+          database: configService.get('TYPEORM_DATABASE') as any,
+          ssl: configService.get('TYPEORM_SSL'),
+          synchronize: true,
+          autoLoadEntities: true,
+          entities: ['dist/**/*.entity.js'],
+          migrations: ['dist/db/migrations/*.js'],
+        }
       },
     }),
     UserModule,
